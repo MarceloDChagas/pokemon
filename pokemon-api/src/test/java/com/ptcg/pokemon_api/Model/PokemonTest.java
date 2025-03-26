88fd8e1f52ed03fcd5065c371d1904169ddbf35b
MarceloDChagas <mlcr1@discente.ifpe.edu.br>
2025-03-26 14:03:40 -0300

    test git hook

--- pokemon-api/src/test/java/com/ptcg/pokemon_api/Model/PokemonTest.java
+++ pokemon-api/src/test/java/com/ptcg/pokemon_api/Model/PokemonTest.java
@@ -18,7 +18,7 @@ public class PokemonTest {
                 .id("1")
                 .name(new Name("Pikachu"))
                 .type(PokemonType.ELECTRIC)
-                .description(new Description("A friendly electric-type Pokémon.")) 
+                .description(new Description(6666); 
                 .cardType(CardType.POKEMON)
                 .rarity(PokemonRarity.COMMON)
                 .evolutionStage(EvolutionStage.BASIC)
